7bc75b9217158bf692fe4a5f59ae4000e01db417
Keslon-1-0 <89145911+Keslon-1-0@users.noreply.github.com>
2022-03-28 15:43:17 +0900

    Add files via upload

---
+++ java/texx.java
@@ -0,0 +1,12 @@
+
+
+public class texx{
+    public static void main(String[] args) {
+            System.out.println("Hello World");
+        
+    
+            
+    }  
+}
+
+// lol pw : a1930671!aktmxj1
