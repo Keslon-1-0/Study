30b98b996da1439c4c36315956f077f9e54a108b
Keslon-1-0 <89145911+Keslon-1-0@users.noreply.github.com>
2022-04-07 21:29:10 +0900

    Add files via upload

--- java/carpark.java
+++ java/carpark.java
@@ -10,12 +10,15 @@ public class carpark
 {
     //3 fields
     //2 int
-    //how many cars can be parked in the car park)
-    int occupancy;
-    //How many cars are currently parked in the car park
-    int capacity;
     //1 string
     String location;
+    //How many cars are currently parked in the car park
+    int capacity;
+    //how many cars can be parked in the car park)
+    int occupancy;
+    
+    //1 string
+    
     
     /**
      * Constructor for objects of class carpark
@@ -36,16 +39,77 @@ public class carpark
      * Returns the capacity of the car park
      */
     public int getCapacity(){
+
         return capacity;
     }
     /**
      * Returns the occupancy of the car park
      */
     public int getOccupancy(){
+        
         return occupancy;
+        
     }
+    public void park(){
+        if(occupancy < capacity){
+            occupancy ++;
+        }
+        else{
+            System.out.println("you cannot park" +
+                                "the car park is full");
+        }
+        
+        
+    }
+    /**
+     * Decreases the occupancy of car park by 1
+     * if occupancy > 0 
+     * otherwise prints error message
+     */
+    public void leave(){
+        if(occupancy > 0){
+            occupancy --;
+        }
+        else{
+            System.out.println("The car is empty");
+            System.out.println("so no car can leave");
+        }
+    }
+    /**
+     * Changes the capacity(up or down) of car park
+     */
+    public void changecapacity(int changeincapacity){
+        //basic case
+        int newcapacity = capacity + changeincapacity;
+        //special case
+        //if occupancy > 0 &&(and)
+        //(capacity + changeincapacity) < occupancy
+        //then do not change capacity and print error messege
+        if(occupancy > 0 &&(capacity + changeincapacity) < occupancy ){
+            System.out.println("New capacity is less than current occupancy");
+            System.out.println(" , so cannot update messege");
+        }
+        else if(occupancy == 0 && (capacity + changeincapacity) < 0){
+            capacity = 0;
+            System.out.println("the car park is closed");
+        }
+        else{
+            //basic case
+            capacity = capacity + changeincapacity;
+        }
+    }
+    /**
+         * Prints the details about car park
+        */
+    public void printDetails(){
+            System.out.println(location +"car park has " +
+                                (capacity - occupancy) + " spaces.");
+    }
+        
+        
     
     
+    //occupancy always <= capacity
     
     
     
