bd5e2742398f856c26156a19105f6c28f8600ba4
Keslon-1-0 <89145911+Keslon-1-0@users.noreply.github.com>
2022-05-17 13:55:55 +0900

    Add files via upload

---
+++ Matlab/exxxx217.m
@@ -0,0 +1,17 @@
+clear
+%(0 100 101 ) dx = 1
+x=linspace(0,10,101);
+dx=0.1
+        %해석해
+ya=exp(-1/2*x)*2/sqrt(3).*sin(sqrt(3)/2*x);
+y1(1)=0;y2(1)=1;
+for i = 1: 100
+    %그대로
+    y1(i+1)=y1(i)+y2(i)*dx;
+                   %바꿀부분 
+    y2(i+1)=y2(i)+(-y2(i)-y1(i))*dx;
+end
+plot(x,ya,'k-')
+hold on
+plot(x,y1,'r-')
+ㄴhold off
