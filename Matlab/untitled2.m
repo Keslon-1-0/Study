059b116d35d094a135c1e6ab379b15fa4b863f86
Keslon-1-0 <89145911+Keslon-1-0@users.noreply.github.com>
2022-05-31 13:41:48 +0900

    Add files via upload

--- Matlab/untitled2.m
+++ Matlab/untitled2.m
@@ -1,15 +1,25 @@
 clear
-x=linspace(0,2,101);
-dx=0.02;
+x=linspace(0,1,101);
+dx=0.01;
 %해석해
-ya=9/2*exp(x)+5/4*exp(2*x)+1/2*x.^2+3/2*x+7/4;
-y1(1)=0;
-y2(1)=1;
-for i =1:100
-    y1(i+1)=y1(i)+y2(i)*dx;
-    y2(i+1)=y2(i)+(x(i)^2+3*y2(i)-2*y1(i))*dx;
+a=1/dx^2/(2/dx^2+3/dx-2);
+b=(1/dx^2+3/dx)/(2/dx^2+3/dx-2);
+ya=1/(exp(1)-exp(2))*(exp(x)-exp(2*x));
+y(1)=0;y(101)=1;y(2:100)=0.5;
+yo=y;
+for k = 1:100000
+    for i = 2 : 100
+        y(i)=a*yo(i+1)+b*yo(i-1);
+    end
+    aydif=abs(y-yo);
+    maxaydif=max(aydif);
+    if maxaydif < 1e-7
+        break
+    end
+    yo=y;
 end
+
 plot(x,ya,'k--')
 hold on
-plot(x,y1,'r--')
+plot(x,y,'r--')
 hold off
